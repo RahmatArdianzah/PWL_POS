abd5cad2ccfecffb2eb75be872e8d28615b41d45
Ardianz <2131710113@student.polinema.ac.id>
2024-03-26 13:18:14 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
+        /*$data = [
             'level_id' => 2,
             'username' => 'manager_tiga',
             'nama' => 'manager 3',
@@ -32,7 +32,17 @@ class UserController extends Controller
         UserModel::insert($data);*/
 
 
-        $user = UserModel::all();
+        /*$user = UserModel::all();
+        return view('user', ['data' => $user]);*/
+        /*$user = UserModel::find(1);
+        return view('user', ['data' => $user]);*/
+        //$user = UserModel::where('level_id', 1)->first();
+        //return view('user', ['data' => $user]);
+        //$user = UserModel::firstWhere('level_id', 1);
+        //return view('user', ['data' => $user]);
+        //$user = UserModel::findOr(1, ['username', 'nama'], function (){abort(404);});
+        //return view('user', ['data' => $user]);
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){abort(404);});
         return view('user', ['data' => $user]);
     }
 }
